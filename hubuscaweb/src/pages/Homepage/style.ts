7b86d78803416b5660bb39b6110fbd0f6175dd72
Winnie Brandão <106097591+Winniebran@users.noreply.github.com>
2024-07-07 10:07:29 -0300

    Delete hubuscaweb directory

--- hubuscaweb/src/pages/Homepage/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledHomePage = styled.main`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: var(--gap-60);
-`;
--- hubuscaweb/src/services/index.tsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const ApiRequests = axios.create({
-  baseURL: "https://api.github.com",
-  timeout: 15000,
-});
